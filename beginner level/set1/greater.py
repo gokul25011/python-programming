9f942f92f49d1cb6d356989e3d3b63f94a022b5a
gokul25011 <37371683+gokul25011@users.noreply.github.com>
2018-03-19 15:35:50 +0530

    Update greater.py

--- beginner level/set1/greater.py
+++ beginner level/set1/greater.py
@@ -1,9 +1,9 @@
-a=raw_input()
+e=raw_input()
 b=raw_input()
 c=raw_input()
-if(a>b and a>c):
-	print("a is greater")
-elif(b>a and b>c):
+if(e>b and e>c):
+	print("e is greater")
+elif(b>e and b>c):
 	print( "b is greater")
-elif(c>b and c>a):
+elif(c>b and c>e):
 	print( "c is greater")
